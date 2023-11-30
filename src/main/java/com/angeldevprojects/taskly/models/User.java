b677810c20cacf69aef9beda744d54f191474086
Ángel Alberto de los Ríos Navarro <angeldevprojects@gmail.com>
2023-11-30 11:12:40 +0100

    Username field unique

--- src/main/java/com/angeldevprojects/taskly/models/User.java
+++ src/main/java/com/angeldevprojects/taskly/models/User.java
@@ -25,7 +25,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     @Size(max = 15)
     private String username;
 
