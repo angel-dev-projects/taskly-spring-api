71ccdee096a1407ce42e14efa84c9110a07aa787
Ángel Alberto de los Ríos Navarro <angeldevprojects@gmail.com>
2024-03-14 19:37:12 +0100

    Update cors origin

--- src/main/java/com/angeldevprojects/taskly/config/WebConfig.java
+++ src/main/java/com/angeldevprojects/taskly/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://angel-dev-projects.github.io/taskly-angular-front/");
+        config.addAllowedOrigin("*");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
