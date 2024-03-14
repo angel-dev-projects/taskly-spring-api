b6290616168664b0f84141f6b3d57660fec0a8c2
Ángel Alberto de los Ríos Navarro <angeldevprojects@gmail.com>
2024-03-14 19:45:42 +0100

    Change properties

--- src/main/java/com/angeldevprojects/taskly/config/WebConfig.java
+++ src/main/java/com/angeldevprojects/taskly/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("https://angel-dev-projects.github.io");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
